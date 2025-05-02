256bd79d5c5f4317cd969e42a007e4d1cd03f506
Abhinash Gupta <97685347+Abhinash01@users.noreply.github.com>
2025-05-02 17:50:46 +0000

    Scripting Done

---
+++ script.js
@@ -0,0 +1,8 @@
+let count = 0;
+const countDisplay = document.getElementById('count');
+const button = document.getElementById('clickBtn');
+
+button.addEventListener('click', () => {
+  count++;
+  countDisplay.textContent = count;
+});
